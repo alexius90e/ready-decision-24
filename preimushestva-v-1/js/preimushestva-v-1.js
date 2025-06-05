b5a8d79cc1bbf465766bb313f0e5d121a1f2dce8
Aliaksei Karzhou <alexius90e@gmail.com>
2025-06-05 13:53:58 +0300

    fix: fixed preimushestva-v-1

--- preimushestva-v-1/js/preimushestva-v-1.js
+++ preimushestva-v-1/js/preimushestva-v-1.js
@@ -1,28 +1,36 @@
-const preimushestvaV1SwiperEl = document.querySelector('.preimushestva-v-1__slider .swiper')
+const preimushestvaV1SwiperEl = document.querySelector('.preimushestva-v-1__slider .swiper');
 
 if (preimushestvaV1SwiperEl) {
   const preimushestvaV1Swiper = new Swiper(preimushestvaV1SwiperEl, {
-  spaceBetween: 15,
-      slidesPerView: 1,
-
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1.2,
-      spaceBetween: 15
+    spaceBetween: 24,
+    slidesPerView: 4,
+    allowTouchMove: false,
+    breakpoints: {
+      480: {
+        slidesPerView: 1.2,
+        spaceBetween: 15,
+        allowTouchMove: true,
+      },
+      599: {
+        slidesPerView: 2.25,
+        spaceBetween: 20,
+        allowTouchMove: true,
+      },
+      899: {
+        slidesPerView: 2.8,
+        spaceBetween: 20,
+        allowTouchMove: true,
+      },
+      1024: {
+        slidesPerView: 3.5,
+        spaceBetween: 20,
+        allowTouchMove: true,
+      },
+      1228: {
+        slidesPerView: 4,
+        spaceBetween: 24,
+        allowTouchMove: false,
+      },
     },
-    599: {
-      slidesPerView: 2.25,
-      spaceBetween: 30
-    },
-    899: {
-      slidesPerView: 3.5,
-      spaceBetween: 20
-    },
-    1228: {
-      slidesPerView: 4,
-      spaceBetween: 24
-    }
-  }
-})
+  });
 }
