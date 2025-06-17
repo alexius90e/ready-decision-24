b41d4c21303929f0b0d39298030eda19c2e7ff3b
Aliaksei Karzhou <alexius90e@gmail.com>
2025-06-17 17:21:05 +0300

    feat: added proekty-v-4

--- js/mod-okno-v-1.js
+++ js/mod-okno-v-1.js
@@ -3,7 +3,8 @@ const modOknoV1Els = document.querySelectorAll('.mod-okno-v-1');
 modOknoV1Els.forEach((modal) => {
   modal.addEventListener('click', (event) => {
     const isLayout = event.currentTarget === event.target;
-    const isClose = event.target.classList.contains('okno-spasibo-v-1__close-button');
+    const isClose = event.target.classList.contains('mod-okno-v-1__close-button');
+
     if (isLayout || isClose) modal.classList.remove('active');
   });
 });
@@ -16,7 +17,19 @@ modOknoV1ButtonEls.forEach((button) => {
 
     if (targetModalId) {
       const modal = document.getElementById(targetModalId);
-      if (modal) modal.classList.add('active');
+
+      if (modal) {
+        modal.classList.add('active');
+        const modalContent = modal.querySelector('.mod-okno-v-1__content');
+
+        if (modalContent) {
+            console.log(modalContent.scrollHeight > modalContent.clientHeight);
+
+          if (modalContent.scrollHeight > modalContent.clientHeight) {
+            modalContent.classList.add('has-scroll');
+          }
+        }
+      }
     }
   });
 });
