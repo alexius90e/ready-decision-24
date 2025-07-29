c362b8d23b8428bee73e4ae8696412acda3e634b
Aliaksei Karzhou <alexius90e@gmail.com>
2025-07-29 22:43:14 +0300

    feat: added taby-v-2

---
+++ js/taby-v-2.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.taby-v-2 .swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 15,
+  breakpoints: {
+    320: {
+      spaceBetween: 15,
+    },
+    1020: {
+      spaceBetween: 20,
+    },
+  },
+});
