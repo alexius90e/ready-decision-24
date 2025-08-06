69254509b9c47549121125822352b9edb9062cc3
Aliaksei Karzhou <alexius90e@gmail.com>
2025-08-07 00:29:28 +0300

    feat: added vidzhet-modok-v-1

--- js/vidzhet-modok-v-1.js
+++ js/vidzhet-modok-v-1.js
@@ -1,10 +1,26 @@
 const vidzhetModokV1El = document.querySelector('.vidzhet-modok-v-1');
 const vidzhetModokV1Delay = 10000;
+const tenMinutesInMs = 1 * 60 * 1000;
+
+function showVidzhetModokV1() {
+  if (vidzhetModokV1El) {
+    vidzhetModokV1El.classList.add('active');
+  }
+}
 
 if (vidzhetModokV1El) {
-  setTimeout(() => {
-    vidzhetModokV1El.classList.add('active');
-  }, vidzhetModokV1Delay);
+  const lastShowTime = localStorage.getItem('vidzhetModokV1ShowTime');
+
+  if (lastShowTime === null) {
+    setTimeout(() => showVidzhetModokV1(), vidzhetModokV1Delay);
+    console.log(vidzhetModokV1Delay);
+  } else if (new Date().getTime() - Number(lastShowTime) >= tenMinutesInMs) {
+    setTimeout(() => showVidzhetModokV1(), vidzhetModokV1Delay);
+    console.log(vidzhetModokV1Delay);
+  } else {
+    setTimeout(() => showVidzhetModokV1(), new Date().getTime() - Number(lastShowTime));
+    console.log(new Date().getTime() - Number(lastShowTime));
+  }
 
   vidzhetModokV1El.addEventListener('click', (event) => {
     const isLayout = event.target === event.currentTarget;
@@ -12,6 +28,7 @@ if (vidzhetModokV1El) {
 
     if (isLayout || isClose) {
       vidzhetModokV1El.classList.remove('active');
+      localStorage.setItem('vidzhetModokV1ShowTime', new Date().getTime());
     }
   });
 }
